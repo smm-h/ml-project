f7b6127bd2b17c53747eafc9eb915a3ad5cec7bd
SMMH <smmh72@gmail.com>
2024-04-23 13:32:53 +0200

    added a class just for Float stats

--- Stats.kt
+++ Stats.kt
@@ -1,55 +1,62 @@
-import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
-class Stats(
-    private val findCount: Boolean = false,
-    private val findSum: Boolean = false,
-    private val findAverage: Boolean = false,
-    private val findMin: Boolean = false,
-    private val findMax: Boolean = false,
-    private val findRange: Boolean = false,
-) {
-    private val calculateCount = findCount || findAverage
-    private val calculateSum = findSum || findAverage
-    private val calculateMin = findMin || findRange
-    private val calculateMax = findMax || findRange
+sealed class Stats {
+    class Doubles : Stats() {
+        var count: Int = 0
+            private set
+        var sum: Double = 0.0
+            private set
+        var min: Double = Double.MAX_VALUE
+            private set
+        var max: Double = Double.MIN_VALUE
+            private set
+        val average: Double
+            get() = sum / count
+        val range: Double
+            get() = max - min
 
-    var count: Int = 0
-        private set
-    var sum: Double = 0.0
-        private set
-    var min: Double = Double.MAX_VALUE
-        private set
-    var max: Double = Double.MIN_VALUE
-        private set
-    val average: Double
-        get() = sum / count
-    val range: Double
-        get() = max - min
+        fun check(it: Double) {
+            count++
+            sum += it
+            min = min(min, it)
+            max = max(max, it)
+        }
 
-    fun check(it: Double) {
-        if (calculateCount) count++
-        if (calculateSum) sum += it
-        if (calculateMin) min = min(min, it)
-        if (calculateMax) max = max(max, it)
+        fun clear() {
+            count = 0
+            sum = 0.0
+            min = Double.MAX_VALUE
+            max = Double.MIN_VALUE
+        }
     }
 
-    fun getSummary(): String {
-        val s = StringJoiner("; ", "[", "]")
-        if (findCount) s.add("Count: $count")
-        if (findSum) s.add("Sum: $sum")
-        if (findAverage) s.add("Average: $average")
-        if (findMin) s.add("Min: $min")
-        if (findMax) s.add("Max: $max")
-        if (findRange) s.add("Range: $range")
-        return s.toString()
-    }
+    class Floats : Stats() {
+        var count: Int = 0
+            private set
+        var sum: Float = 0f
+            private set
+        var min: Float = Float.MAX_VALUE
+            private set
+        var max: Float = Float.MIN_VALUE
+            private set
+        val average: Float
+            get() = sum / count
+        val range: Float
+            get() = max - min
 
-    fun clear() {
-        count = 0
-        sum = 0.0
-        min = Double.MAX_VALUE
-        max = Double.MIN_VALUE
+        fun check(it: Float) {
+            count++
+            sum += it
+            min = min(min, it)
+            max = max(max, it)
+        }
+
+        fun clear() {
+            count = 0
+            sum = 0f
+            min = Float.MAX_VALUE
+            max = Float.MIN_VALUE
+        }
     }
 }
