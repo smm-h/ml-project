558f94714123abba3104131ef85063db4363335c
SMMH <smmh72@gmail.com>
2024-04-23 13:32:27 +0200

    plotter can now handle multiple plots

--- plotter.py
+++ plotter.py
@@ -1,3 +1,4 @@
+from math import sqrt, ceil
 import matplotlib.pyplot as plt
 
 
@@ -28,24 +29,32 @@ def get_plot_data(timestamp):
     return plot_data
 
 
-# with open("experiments/list.txt") as f:
-#     timestamps = [i.strip() for i in f]
+with open("experiments/list.txt") as f:
+    all_timestamps = [i.strip() for i in f]
 
-timestamps = ["202404111_070518", "202404112_012337"]
+# ["202404111_070518", "202404112_012337"]
+selected_timestamps = [all_timestamps[-1]]  # [-1-(3)**2:-1]
 
-plots = [get_plot_data(i) for i in timestamps]
+n = len(selected_timestamps)
 
-fig, ax = plt.subplots(nrows=2, ncols=2)
+side = ceil(sqrt(n))
 
-for row_id in range(len(ax)):
-    row = ax[row_id]
-    for col_id in range(len(row)):
-        col = row[col_id]
-        data = plots[col_id]
+fig, ax = plt.subplots(nrows=side, ncols=side)
+
+for i in range(n):
+    t = selected_timestamps[i]
+    part = ax
+    if "__getitem__" in dir(part):
+        part = part[i // side]
+        if "__getitem__" in dir(part):
+            part = part[i % side]
+    try:
+        data = get_plot_data(t)
         x = data["TimeElapsed"]
-        col.plot(x, data["Average"], "b")
-        col.plot(x, data["Min"], "r")
-        col.plot(x, data["Max"], "g")
-
+        part.plot(x, data["Average"], "b")
+        part.plot(x, data["Min"], "r")
+        part.plot(x, data["Max"], "g")
+    except FileNotFoundError:
+        pass
 
 plt.show()
