b0dbebfa773c1b2513f399fdfeb8b954d229bd54
SMMH <smmh72@gmail.com>
2024-04-28 13:04:54 +0200

    changed to FlatLaf version 2.6 because the icons on the JTree were not visible

--- src/main/gui/GUI.kt
+++ src/main/gui/GUI.kt
@@ -4,53 +4,43 @@ import ActivationFunction.Companion.RELU
 import MultilayerPerceptron
 import com.formdev.flatlaf.FlatLightLaf
 import src.main.util.Util.by
-import java.awt.Component
 import java.awt.GridLayout
 import java.util.*
 import javax.swing.*
-import javax.swing.JOptionPane.showMessageDialog
 import javax.swing.tree.DefaultMutableTreeNode
-import javax.swing.tree.DefaultTreeCellRenderer
 import javax.swing.tree.DefaultTreeModel
 import javax.swing.tree.TreeModel
 
 
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 class GUI {
-    private object CustomCellRenderer : DefaultTreeCellRenderer() {
-        override fun getTreeCellRendererComponent(
-            tree: JTree?,
-            value: Any?,
-            sel: Boolean,
-            expanded: Boolean,
-            leaf: Boolean,
-            row: Int,
-            hasFocus: Boolean
-        ): Component {
-            super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus)
-            icon = UIManager.getIcon("FileView.directoryIcon")
-            return this
-        }
-    }
-
-    val root = DefaultMutableTreeNode("Root").apply {
+    val root = DefaultMutableTreeNode().apply {
         this.add(DefaultMutableTreeNode("Example"))
     }
     val treeModel: TreeModel = DefaultTreeModel(root)
-    val tree = JTree(treeModel).apply {
-        cellRenderer = CustomCellRenderer
+    val tree = JTree().apply {
     }
     val treePanel = JPanel(GridLayout()).apply {
+//        FlatLightLaf.initIconColors()
+//        tree.putClientProperty("Tree.background", "#ff0000")
+//        tree.putClientProperty("Tree.background", Color.RED)
+//        tree.putClientProperty("background", "#ff0000")
+//        tree.putClientProperty("background", Color.RED)
+//        tree.putClientProperty("JTree.paintSelection", "true")
+//        tree.putClientProperty("JTree.paintSelection", true)
+//        tree.putClientProperty("Tree.paintSelection", "true")
+//        tree.putClientProperty("Tree.paintSelection", true)
 //        tree.isRootVisible = false
-        tree.addTreeSelectionListener {
-            if (it.isAddedPath) {
-                showMessageDialog(null, it.path)
-            }
-        }
+//        tree.addTreeSelectionListener {
+//            if (it.isAddedPath) {
+//                showMessageDialog(null, it.path)
+//            }
+//        }
+//        tree.showsRootHandles = true
+//        tree.putClientProperty("Tree.showDefaultIcons", true)
         add(tree)
     }
     val tabs = JTabbedPane().apply {
-
     }
     val tabsPanel = JPanel(GridLayout()).apply {
         add(tabs)
@@ -58,6 +48,7 @@ class GUI {
     val split = JSplitPane().apply {
         leftComponent = treePanel
         rightComponent = tabsPanel
+        this.dividerLocation = 192
     }
     val panel = JPanel(GridLayout()).apply {
         add(split)
@@ -69,28 +60,28 @@ class GUI {
         contentPane = panel
     }
 
-    private val installedSkins: Map<String, String> by lazy {
-        UIManager.getInstalledLookAndFeels()
-            .map { it.className }
-            .associateBy { it.slice(it.lastIndexOf(".") + 1..it.length - 12) }
-    }
-
     companion object {
-        private const val DEFAULT_SKIN_NAME = "Default"
+
+        private val installedSkins: Map<String, String> by lazy {
+            UIManager.getInstalledLookAndFeels()
+                .map { it.className }
+                .associateBy { it.slice(it.lastIndexOf(".") + 1..it.length - 12) }
+        }
 
         val INSTANCE by lazy { GUI() }
 
         @JvmStatic
         fun main(args: Array<String>) {
 
-            FlatLightLaf.setup()
-
             val structure = MultilayerPerceptron.Structure(10, 10, listOf(10))
             val blueprint = MultilayerPerceptron.Blueprint(structure, RELU, listOf(RELU))
             val model = blueprint.instantiate().also { it.randomize(Random()) }
 
+            FlatLightLaf.setup()
+//            UIManager.setLookAndFeel(installedSkins["Nimbus"])
+            UIManager.setLookAndFeel(FlatLightLaf())
+
             INSTANCE.apply {
-                UIManager.setLookAndFeel(FlatLightLaf())
                 tabs.addTab("new-tab", MLPUI(model))
                 frame.apply {
                     preferredSize = 640 by 480
