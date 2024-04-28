ae9651b9980706b02d508ec76a491205d7c96904
SMMH <smmh72@gmail.com>
2024-04-28 10:50:39 +0200

    made parts globally available

--- src/test/mlp/TinyModelTest.kt
+++ src/test/mlp/TinyModelTest.kt
@@ -4,24 +4,22 @@ import ActivationFunction.Companion.RELU
 import MultilayerPerceptron.Blueprint
 import MultilayerPerceptron.Companion.readModel
 import MultilayerPerceptron.Structure
+import java.io.File
 import java.util.*
 
 object TinyModelTest {
+
+    val structure = Structure(1, 1, listOf(1))
+    val blueprint = Blueprint(structure, RELU, listOf(RELU))
+    val model = blueprint.instantiate()
+
     @JvmStatic
     fun main(args: Array<String>) {
         val filename = "tiny.mlp"
         val random = Random()
-
-        Blueprint(
-            Structure(1, 1, listOf(1)),
-            RELU,
-            listOf(RELU),
-        ).instantiate().also {
-            it.randomize(random)
-            it.writeToFile(filename)
-            assert(it == readModel(filename))
-        }
-
-//        File(filename).delete()
+        model.randomize(random)
+        model.writeToFile(filename)
+        assert(model == readModel(filename))
+        File(filename).delete()
     }
 }
